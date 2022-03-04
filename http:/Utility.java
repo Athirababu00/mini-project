08711955e2dad5de49a980d67752cf2003c5b98b
Athirababu00 <84671375+Athirababu00@users.noreply.github.com>
2022-03-04 22:13:30 +0530

    Create Utility.java

---
+++ http:/Utility.java
@@ -0,0 +1,10 @@
+package com.syntax.petvet;
+
+public class Utility {
+
+    public static String ip = "10.100.30.108";
+
+    public static String SERVERUrl = "http://"+ip.trim()+":8080/PetVet_Server/ServerPV/petvet_server.jsp";
+
+
+}
